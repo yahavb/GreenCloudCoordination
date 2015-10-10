5715b821c328f9f013be1d1a91323b0666f4c151
Yahav Biran <ybiran@colostate.edu>
2015-10-10 15:54:05 -0700

    Update create_schema.sql

--- voltdb_setup/create_schema.sql
+++ voltdb_setup/create_schema.sql
@@ -1,1 +1,23 @@
-create demand table
+create demand table{
+id,
+datetime,
+region,
+total_job_workload, 
+load_factor,
+deadline}
+
+create supply table{
+id,
+datetime,
+region,
+type,
+cut-in,
+rated,
+cut-off,
+power-efficiency}
+
+create job table
+id,
+demand_id, 
+supply_id,
+status}
