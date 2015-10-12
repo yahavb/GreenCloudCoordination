8a56d939ada3016bb72158761f625ea2eedb557d
Yahav Biran <ybiran@colostate.edu>
2015-10-12 16:11:40 -0700

    add job, supply demand, and status tables
    add job, supply demand, and status tables

--- voltdb_setup/create_schema.sql
+++ voltdb_setup/create_schema.sql
@@ -1,26 +1,60 @@
-create demand table{
-id,
-datetime,
-region,
-total_job_workload, 
-load_factor,
-deadline,
-required_core_hour}
+CREATE TABLE demand
+(
+  id   varchar(50)    NOT NULL
+, datetime   datetime NOT NULL
+, region  varchar(50) NOT NULL
+, total_job_workload integer     NOT NULL
+, load_factor varchar(50) NOT NULL
+, CONSTRAINT d_id PRIMARY KEY
+  (
+    id
+  )
+);
 
-create supply table{
-id,
-datetime,
-region,
-type,
-cut-in,
-rated,
-cut-off,
-power-efficiency,
-avail_cores_hour}
+CREATE TABLE demand_status
+(
+  demand_id varchar(50) NOT NULL
+  , deadline datetime NOT NULL
+  , required_core_hour integer     NOT NULL
+  , CONSTRAINT pk_ds_id PRIMARY KEY
+  (
+    demand_id
+  )
+);
 
-create job table
-id,
-demand_id, 
-supply_id,
-status,
-core_hour}
+CREATE TABLE supply
+(
+  id    varchar(50)   NOT NULL
+, datetime   datetime NOT NULL
+, region  varchar(50) NOT NULL
+, supply_type varchar(50) NOT NULL
+, CONSTRAINT s_id PRIMARY KEY
+  (
+    id
+  )
+);
+
+CREATE TABLE supply_status
+(
+  supply_id varchar(50) NOT NULL
+  , cut_in integer     NOT NULL
+  , rated integer     NOT NULL
+  , cut_off integer     NOT NULL
+  , CONSTRAINT pk_ss_id PRIMARY KEY
+  (
+    supply_id
+  )
+);
+
+CREATE TABLE job
+(
+  id varchar(50) NOT NULL
+  , demand_id varchar(50) NOT NULL
+  , demand_id varchar(50) NOT NULL
+  , status integer     NOT NULL
+  , core_hours integer     NOT NULL
+  , CONSTRAINT j_id PRIMARY KEY
+  (
+    id
+  )
+);
